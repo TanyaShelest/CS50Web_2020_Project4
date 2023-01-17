73122f0e52c76f319cd4bb8c5320ec189170ac55
Tetiana Shelest <tashelest@gmail.com>
2023-01-17 12:11:21 +0100

    Create and modify models
    1. Post and Like models have been created.
    2. Profile model has been created to reflect followers-following relations
    3. Add string representation for User

--- network/models.py
+++ network/models.py
@@ -1,6 +1,37 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=50, unique=False)
+    last_name = models.CharField(max_length=50, unique=False)
+
+    def __str__(self):
+        return f"{self.id}: {self.first_name} {self.last_name}"
+
+class Profile(models.Model):
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, created, instance, **kwargs):
+        if created:
+            profile = Profile(user=instance)
+            profile.save()
+            profile.follows.set([])
+            profile.save()
+
+    def __str__(self):
+            return self.user.username
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
