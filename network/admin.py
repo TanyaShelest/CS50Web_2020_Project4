9a25a2b132421780d2e89fa5ff07ff7e2527891a
Tetiana Shelest <tashelest@gmail.com>
2023-01-29 20:21:24 +0100

    Register admin models for User, Profile and Post

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, User, Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author")
+    fields = ("author", "title", "body")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Post, PostAdmin)
