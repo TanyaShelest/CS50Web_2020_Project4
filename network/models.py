f3fd41170f54dbb4082c87e91a8a4767c410fe80
Tetiana Shelest <tashelest@gmail.com>
2023-02-17 21:13:45 +0100

    Add str method to Post model

--- network/models.py
+++ network/models.py
@@ -29,6 +29,9 @@ class Post(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.body
+
 class Like(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
